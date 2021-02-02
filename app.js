8e5997e0ab3e976502d02f1d511dd97bc3c07ea6
Bularu Lilian <aryanlilian@gmail.com>
2021-02-02 22:09:14 +0200

    Connected to MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
 
-app = express();
-app.listen(8000);
+const app = express();
+const dbURI = "mongodb+srv://aryanlilian:testingpass@cluster0.wcaui.mongodb.net/node-blog?retryWrites=true&w=majority";
+mongoose.connect(dbURI).then((result) => app.listen(8000)).catch((err) => {console.log(err)});
+
 app.set('view engine', 'ejs')
+
 
 app.use(morgan('dev'))
 app.use(express.static('static'))
@@ -13,13 +18,21 @@ app.get('/', (req, res) => {
 });
 
 app.get('/blog', (req, res) => {
-    const blogs = [
-        {title : 'First post', content : 'The content here'},
-        {title : 'Second post', content : 'The content here'},
-        {title : 'Third post', content : 'The content here'},
-    ];
-    res.render('blog', {title : 'Blog', page_title : 'Blog Page', blogs});
+    Blog.find().sort({ createdAt : -1 }).then((result) => {
+        res.render('blog', {title : 'Blog', page_title : 'Blog Page', blogs : result});
+    }).catch((err) => {
+        console.log(err);
+    });
 });
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+});
+
 
 app.use((req, res) => {
     res.render('404', {title : '404', page_title : 'Page don\'t exist'});
